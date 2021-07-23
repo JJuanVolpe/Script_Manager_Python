cff0d090a2e4bafaedd9017fad5928d84d1493a8
JJuanVolpe <juan.voolpe@gmail.com>
2021-07-23 16:21:35 -0300

    third commit

--- principal.py
+++ principal.py
@@ -13,16 +13,20 @@ def	imprimirDic(D):
 	for items in D:					#Iterando e imprimiendo opciones
 		print(str(items) + " " + D[items])
 
-def generarCampoEnDicc():
-	indice = len(MenudeConsejos) + 1
+
+def generarConsejoenDic():
+	indice = len(MenudeConsejos) + 1			#Asigno indice correspondiente a la opcion número N (agregando 1 que es la opcion entrante)
+	indice = str(indice)						#Casting entrante: longitud del Dicc(int) a String
+	indice = bcolors.OK + indice + "~" + bcolors.RESET			#Detalle de color
 	item = str(input("A continuacion ingrese el consejo que desee agregar al listado:"))
+	item =  bcolors.OK + item + bcolors.RESET					#Detalle de color
 	MenudeConsejos[str(indice)] = item
 
 
 #Proceso de llamado de menu principal
 def callMenu(opciones):
 	imprimirDic(opciones) #Imprime opciones del Menú principal
-	eleccion = str(input("Ingrese la opción por el número correspondiente:\n"))
+	eleccion = str(input("Ingrese la opción por el número correspondiente:"))
 	return eleccion[0]
 
 #Proceso de Login
@@ -30,9 +34,9 @@ def Autenticacion():
 	passw = "brainstorming"
 	tried = 0		#Intentos ingresando password
 	while(tried < 3):
-		hit = str(input(bcolors.LB + "Ingrese la contraseña para ser admitido por el sistema:\n" + bcolors.RESET))
+		hit = str(input(bcolors.LB + "Ingrese la contraseña para ser admitido por el sistema:" + bcolors.RESET))
 		if (hit.__eq__(passw)):		#Usuario reconocido? entonces sale del loop, sino suma un intento
-			print(bcolors.OK + "Bienvenido usuario!\n" + bcolors.RESET)
+			print(bcolors.OK + "\nBienvenido usuario!" + bcolors.RESET)
 			break
 		tried = tried+1
 	return tried
@@ -42,9 +46,9 @@ def Autenticacion():
 
 #	Genero Diccionario del menú principal
 MenudeOpciones = {
-    "1)" : " Teléfonos y contactos útiles por covid:" ,
+    "\n1)" : " Teléfonos y contactos útiles por covid:" ,
     "2)" : " Páginas con más información" ,
-    "3)" : " Consejos y recomendaciones al azar" ,
+    "3)" : " Consejos y recomendaciones época de pandemia" ,
     "4)" : " Agregar consejo y/0 recomendación",
     "5)" : " Salir del programa"
 }
@@ -69,8 +73,8 @@ PaginasWeb = {
 MenudeConsejos = {
     bcolors.OK + "1~" : "El barbijo tiene que cubrirte la nariz, la boca y el mentón" ,
     			 "2~" : "Mantener una buena higiene, tanto personal cómo con el medio" ,
-    			 "3~" : "Evitar Aglomeración de gente o lugares con frecuentados por multitud " ,
-    			 "4~" : "Si no te encuentras bien, quedate en casa" + bcolors.RESET
+    			 "3~" : "Evitar aglomeración de gente o lugares con frecuentados por multitud " ,
+    			 "4~" : "Si no te sentís bien, quedate en casa" + bcolors.RESET
 }
 
 #Generamos una lista de diccionarios para iterar facilmente, generar mayor reusabilidad y generalización de código.
@@ -91,8 +95,8 @@ while (ok):								# Entramos al menú/loop principal de nuestro Script/App.
 		quit()
 	elif (choice== "1" or choice== "2" or choice== "3"):
 		x = int(choice)-1                     #Restamos uno a la eleccion para acceder al índice correcto en la lista de diccionarios.
-		imprimirDic(ListaOpciones[x]);		  #Accedemos a lista de diccionarois
+		imprimirDic(ListaOpciones[x]);		  #Accedemos a lista de diccionarios
 	elif (choice == "4"):
-		generarCampoEnDicc()                                 &/&//&/a pensar mas
+		generarConsejoenDic()
 		print(bcolors.LB + "Consejo Agregado correctamente!" + bcolors.RESET)
 
